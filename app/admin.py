f93516f69cc22205323a171718d2f5ea421b99ab
Latona7533 <97697930+Latona7533@users.noreply.github.com>
2022-04-23 16:24:05 +0300

    add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Profile, Portfolio, Comment, Blog
+
+admin.site.register(Profile)
+admin.site.register(Portfolio)
+admin.site.register(Comment)
+admin.site.register(Blog)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+"""
+2 организация бд:
+	Профиль 
+		фото
+		описание
+	
+	Портфолио
+		картинка
+		описание
+		ссылка
+		сложность(1-5)
+	
+	Комментарии
+		текст
+
+	Блог
+		название
+		текст
+
+"""
+
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='photos/')
+    descr = models.TextField(max_length=50)
+
+
+class Portfolio(models.Model):
+    DIFFICULT = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+
+    ]
+
+    image = models.ImageField(upload_to='photos/')
+    descr = models.TextField(max_length=50)
+    link = models.CharField(max_length=50)
+    difficult = models.CharField(max_length=1, choices=DIFFICULT,default='1',)
+
+
+class Comment(models.Model):
+    project = models.ForeignKey(Portfolio, on_delete=models.CASCADE, default=None)
+    text = models.TextField(max_length=50)
+
+
+class Blog(models.Model):
+    name = models.TextField(max_length=50)
+    text = models.TextField(max_length=50)
