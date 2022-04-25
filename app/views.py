e74e3fc19dbe7238028cf23e9c6cf1ac97b040a6
Latona7533 <97697930+Latona7533@users.noreply.github.com>
2022-04-25 18:55:23 +0300

    add media and templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from app.models import Profile
+
+
+class MainPageView(ListView):
+    model = Profile
+    template_name = 'index.html'
+    context_object_name = 'profile'
+
